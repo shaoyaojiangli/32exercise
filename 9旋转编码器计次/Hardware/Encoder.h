32ed0a8e5171edd2f433c2d502c9408c1b1517ec
shaoyaojiangli <1909914367@qq.com>
2023-09-13 00:34:00 +0800

    9旋转编码器计次

---
+++ 9旋转编码器计次/Hardware/Encoder.h
@@ -0,0 +1,6 @@
+#ifndef __ENCODER_H
+#define __ENCODER_H
+
+int16_t Encoder_Get();
+void Encoder_Init();
+#endif
---
+++ 9旋转编码器计次/User/main.c
@@ -0,0 +1,23 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "Encoder.h"
+
+int16_t Num;
+
+int main()
+{
+    OLED_Init();
+    
+   Encoder_Init();
+    OLED_ShowString(1, 1, "Number:");
+  
+   
+    
+    while(1)
+    {
+        Num += Encoder_Get();
+        OLED_ShowSignedNum(1, 5, Num, 5);
+    }
+}
+
